1b10233903a2334626128d9b4be358d1c4ffc05d
Mico Shigefuji <micosiwang20@gmail.com>
2022-03-05 14:34:35 +0800

    First Commit

---
+++ src/components/LottoGenerator.vue
@@ -0,0 +1,66 @@
+<template>
+  
+    <div class="container">
+        <img src="../assets/logo.png" alt="logo">
+        <h1>Lotto Generator</h1>
+    </div>
+    <div class="container">
+        <button @click="generate()">Generate Number</button>
+    </div>
+   <div class="container">
+        <div class="output" v-for="digit in arrayNumber" :key="digit">
+            {{ digit }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            arrayNumber : []
+        }
+    },
+    methods: {
+
+       generate(){
+            
+            while(this.arrayNumber.length < 6){
+                let digits = Math.floor((Math.random() * 41))+1;
+                if(!this.arrayNumber.includes(digits)){
+                    this.arrayNumber.push(digits);
+                }
+            }
+            
+        }
+
+    }
+
+}
+</script>
+
+<style scope>
+
+
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img{
+        height: 50px;
+    }
+
+    button{
+        padding: 10px;
+    }
+    .output{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+    }
+
+    
+</style>
